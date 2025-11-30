be0b27e0518f89060f5d10f3801cbaa0ac14ac55
vanshsharma69 <vanshsharma1750@gmail.com>
2025-11-30 13:26:14 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='flex justify-center text-xl items-center h-64'>Hi</div>
+    </>
+  )
+}
+
+export default App
